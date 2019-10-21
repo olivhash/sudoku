c842026e81f565e31d2fe07ff6627c8ab1e0026f
olivier <olivier@leningrad>
2019-10-21 20:22:29 +0200

    Initial Commit

---
+++ src/Tricolore.java
@@ -0,0 +1,16 @@
+enum CouleurFeu {
+	VERT, JAUNE, R
+}
+
+public class Tricolore {
+	public static void main(String[] args) {
+		int tmp = 1;
+		System.out.println(fois(2,3));
+		System.out.println(tmp);
+	}
+	public static int fois(double x, double y) {
+		int txxxxxssmp = 3;
+		return txxxxxssmp;
+	}
+}
+
