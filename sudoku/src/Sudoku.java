32c997b47539d1ceb8f8cb2e18118ff2353017f2
Olivier Hermant <olivier.hermant@mines-paristech.fr>
2019-10-21 19:33:31 +0200

    Split des cases dans un nouveau fichier

--- sudoku/src/Sudoku.java
+++ sudoku/src/Sudoku.java
@@ -1,40 +1,4 @@
 
-class Case {
-	private int ligne;
-	private int colonne;
-
-	public int getLigne() {
-		return ligne;
-	}
-
-	public int getColonne() {
-		return colonne;
-	}
-
-	Case(int l, int c) {
-		this.ligne = l;
-		this.colonne = c;
-	}
-
-	Case() {
-		this(0, 0);
-	}
-
-	/* Suivante(8,8) est (9,0) */
-	Case suivante() {
-		Case c = new Case();
-		if (this.colonne == 8) {
-			c.colonne = 0;
-			c.ligne = this.ligne + 1;
-		} else {
-			c.ligne = this.ligne;
-			c.colonne = this.colonne + 1;
-		}
-		return c;
-	}
-
-}
-
 class Grille {
 	private int[][] grille;
 	private long appels;
